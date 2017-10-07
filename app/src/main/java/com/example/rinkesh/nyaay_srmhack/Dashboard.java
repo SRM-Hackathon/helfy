6e0a589af966090aaba997bda40005179e547418
RinkeshG <saviorindiaservice@gmail.com>
2017-10-07 09:05:33 +0530

    twelve Commit
    12th commit

--- app/src/main/java/com/example/rinkesh/nyaay_srmhack/Dashboard.java
+++ app/src/main/java/com/example/rinkesh/nyaay_srmhack/Dashboard.java
@@ -1,9 +1,15 @@
 package com.example.rinkesh.nyaay_srmhack;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,14 +19,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.UUID;
 
 public class Dashboard extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener , View.OnClickListener {
+
+ /*   private Button buttonChoose;
+    private Button buttonUpload;
+
+    private EditText editText;
+
+    public static final String UPLOAD_URL = "";
+
+    private int PICK_PDF_REQUEST = 1;
+
+    private static final int STORAGE_CODE_PERMISSION = 101;
+
+    private Uri filePath;  */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        requestStoragePermission();
+
+      /*  buttonChoose = (Button) findViewById(R.id.button);
+        buttonUpload = (Button) findViewById(R.id.button);
+
+        editText = (EditText) findViewById(R.id.editcourt);
+
+        buttonChoose.setOnClickListener(this);
+        buttonUpload.setOnClickListener(this);  */
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,7 +64,7 @@ public class Dashboard extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Dashboard.this,Bot.class);
+                Intent i = new Intent(Dashboard.this, Bot.class);
                 startActivity(i);
             }
         });
@@ -41,6 +77,9 @@ public class Dashboard extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void requestStoragePermission() {
     }
 
     @Override
@@ -107,4 +146,89 @@ public class Dashboard extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+   /* public void uploadMultiPart() {
+
+        String name = editText.getText().toString().trim();
+
+        String path = FilePath.getPath(this, filePath);
+
+        if (path == null) {
+
+            Toast.makeText(this, "Please move your .pdf file to internal storage and retry", Toast.LENGTH_LONG).show();
+        } else {
+            try {
+                String uploadId = UUID.randomUUID().toString();
+
+                new MultiPartUploadRequest(this, uploadId, UPLOAD_URL)
+                        .addFileToUpload(path, "pdf")
+                        .addParameter("name", name)
+                        .setNotificationConfig(new UploadNotificationConfig())
+                        .setMaxRetries(2)
+                        .startUpload();
+            } catch (Exception e) {
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void showFileChooser() {
+
+        Intent intent = new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Pdf"), PICK_PDF_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_PDF_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+        }
+    }
+
+    private void requestStoragePermission() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            return;
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+        }
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+
+
+        }
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                Toast.makeText(this, "Permission granted now you can read the storage", Toast.LENGTH_LONG).show();
+            } else {
+
+                Toast.makeText(this, "Oops you just denied the permission", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    */
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+
+
+
 }
